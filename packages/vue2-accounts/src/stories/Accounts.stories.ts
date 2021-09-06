dea8477630f3f63f314534df3c19396cb17aebdf
Kyle Finley <Kyle@KyleFinley.net>
2021-09-06 16:56:32 +0000

    CI: Actions test

--- packages/vue2-accounts/src/stories/Accounts.stories.ts
+++ packages/vue2-accounts/src/stories/Accounts.stories.ts
@@ -1,51 +1,53 @@
-import Vuex from 'vuex';
-import { Story } from '@storybook/vue/types-6-0';
+import Vuex from "vuex";
+import { Story } from "@storybook/vue/types-6-0";
 import Accounts from "@/components/Accounts.vue";
-import { AccountsStatus, initializeModules } from '@/store';
+import { AccountsStatus, initializeModules } from "@/store";
 import { initializeModules as initializeNotifications } from "@platform8/vue2-notify/src/store";
 import { Notify } from "@platform8/vue2-notify/src/components";
-import { AddAccountRequest, AddAccountResponse } from '@/models';
-import { Command } from '@platform8/commands/src';
-import { AddAccountCommand } from '@/commands';
-import { container, injectable } from 'inversify-props';
-import NotificationModule from '@platform8/vue2-notify/src/store/notificationModule';
-import { AccountsModule } from '@/store/accountsModule';
+import { AddAccountRequest, AddAccountResponse } from "@/models";
+import { Command } from "@platform8/commands/src";
+import { AddAccountCommand } from "@/commands";
+import { container } from "inversify-props";
+import NotificationModule from "@platform8/vue2-notify/src/store/notificationModule";
+import { AccountsModule } from "@/store/accountsModule";
 
-let store = new Vuex.Store({
-  plugins: [
-    initializeModules,
-    initializeNotifications,
-  ],
+const store = new Vuex.Store({
+  plugins: [initializeModules, initializeNotifications],
   modules: {
-    "Notification": NotificationModule,
-    "Accounts": AccountsModule,
+    Notification: NotificationModule,
+    Accounts: AccountsModule,
   }
 });
 
-class mockAddAccountCommand implements Command<AddAccountRequest, AddAccountResponse> {
+class mockAddAccountCommand
+  implements Command<AddAccountRequest, AddAccountResponse>
+{
   public async runAsync(login: AddAccountRequest): Promise<AddAccountResponse> {
     // Quick sleep to simulate api call
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       resolve({
-        success: true
-      } as AddAccountResponse)
+        success: true,
+      } as AddAccountResponse);
     });
   }
 }
 
-container.addTransient<AddAccountCommand>(mockAddAccountCommand, "AddAccountCommand");
+container.addTransient<AddAccountCommand>(
+  mockAddAccountCommand,
+  "AddAccountCommand"
+);
 
 export default {
-  title: 'Components/Accounts/Accounts',
+  title: "Components/Accounts/Accounts",
   component: Accounts,
 };
 
 const DefaultTemplate: Story = (args, { argTypes }) => ({
   components: { Accounts, Notify },
   store,
-  template: '<div><notify /><accounts /></div>'
+  template: "<div><notify /><accounts /></div>"
 });
 
 export const Default = DefaultTemplate.bind({});
@@ -62,7 +64,7 @@ const LoadingTemplate: Story = (args, { argTypes }) => ({
       }
     }
   }),
-  template: '<accounts />'
+  template: "<accounts />"
 });
 
 export const Loading = LoadingTemplate.bind({});
